9d6c1595c29b9f5171d633fb1c64cb3f51ee96a6
Andrew <andrew.r.pelton@gmail.com>
2026-08-12 14:41:31 -0700

    updated labels with the correct text

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import tkinter as tk
 import json
+
+days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"]
 
 class Block():
     def __init__(self, name:str, startTime:datetime, endTime:datetime, color, tags:list[str]) -> None:
@@ -29,8 +31,10 @@ class Tile():
         self.row = row
         self.column = column
 
+def generate_tiles(root: tk.Tk, gridWidth: int, gridHeight: int, tileWidth:int, tileHeight:int, startTime:int, timeInterval:int):
+    start_time = datetime(2000, 1, 1, startTime, 0)  # arbitrary date, just care about time
+    times = [start_time + timedelta(minutes=timeInterval * i) for i in range(gridHeight - 1)]
 
-def generate_tiles(root: tk.Tk, gridWidth: int, gridHeight: int, tileWidth:int, tileHeight:int):
     for y in range(gridHeight):
         for x in range(gridWidth):
             box = tk.Frame(root, width=tileWidth, height=tileHeight, bg="gray",
@@ -40,22 +44,19 @@ def generate_tiles(root: tk.Tk, gridWidth: int, gridHeight: int, tileWidth:int, 
             t = Tile(Block("", datetime.now(), datetime.now(), "gray", ["#empty"]), box, x, y)
 
             if x == 0 and y != 0:
-                lbl = tk.Label(box, text="10:00", padx=0, pady=0, bg="gray")
+                lbl = tk.Label(box, text=times[y-1].strftime("%I:%M"), padx=0, pady=0, bg="gray")
                 lbl.place(relx=0.5, rely=0.5, anchor="center")
 
             if y == 0 and x != 0:
-                lbl = tk.Label(box, text="Mon", padx=0, pady=0, bg="gray")
+                lbl = tk.Label(box, text=days[x-1], padx=0, pady=0, bg="gray")
                 lbl.place(relx=0.5, rely=0.5, anchor="center")
 
     root.update_idletasks()
     root.update()
-            
-
 
 root = tk.Tk()
 root.title("Time Blocking App")
 
-generate_tiles(root, 8, 33, 60, 25)
-
+generate_tiles(root, 8, 33, 60, 25, 6, 30)
 
 root.mainloop()
