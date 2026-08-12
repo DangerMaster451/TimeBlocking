bfaaf04c32d06fb52cb500fbbaea2be21ebaebb1
Andrew <andrew.r.pelton@gmail.com>
2026-08-12 12:09:08 -0700

    adjusted the json format and includes tags

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ from datetime import datetime
 import json
 
 class Block():
-    def __init__(self, name:str, startTime:datetime, endTime:datetime, color) -> None:
+    def __init__(self, name:str, startTime:datetime, endTime:datetime, color, tags:list[str]) -> None:
         self.name:str = name
         self.startTime:datetime = startTime
         self.endTime:datetime = endTime
         self.color = color
+        self.tags = tags
+
+    def toJSON(self) -> str:
+        return json.dumps({"name": self.name, "startTime": self.startTime.strftime("%Y-%m-%d %H:%M:%S"), 
+                           "endTime": self.endTime.strftime("%Y-%m-%d %H:%M:%S"), "color": self.color,
+                           "tags": self.tags})
 
     @classmethod
     def fromJSON(cls, json_str:str):
         data = json.loads(json_str)
-        return cls(name=data["name"], startTime=data["startTime"], endTime=data["endTime"], color=data["color"])
+        format = "%Y-%m-%d %H:%M:%S"
+        return cls(data["name"], datetime.strptime(data["startTime"], format),
+                   datetime.strptime(data["endTime"], format), data["color"], data["tags"])
 
-    def toJSON(self) -> str:
-        return f'{{"name":"{self.name}", "startTime":"{self.startTime}", "endTime":"{self.endTime}", "color":"{self.color}"}}'
+   
 
-b = Block("Band", datetime(2000, 1, 1, 0, 0, 0), datetime(2000, 1, 1, 1, 0, 0), "white")
+b = Block("Band", datetime(2000, 1, 1, 0, 0, 0), datetime(2000, 1, 1, 1, 0, 0), "white", ["#busy"])
 s = b.toJSON()
+print(s)
 c = Block.fromJSON(s)
-print(c.startTime)
+print(c.tags)
